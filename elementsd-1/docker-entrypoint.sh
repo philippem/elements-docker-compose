91bf091582ed01e046530c87bc5487ef2cdd47c8
Philippe McLean <philippe.mclean@gmail.com>
2022-08-26 12:28:43 -0700

    Add instructions for installing docker as pre-req
    - add linux postinstall instructions
    - add pointers to elements project tutorial

--- elementsd-1/docker-entrypoint.sh
+++ elementsd-1/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 wait-for-it.sh --host=bitcoind --port=18888 --timeout=0
-wait-for-it.sh --host=elementsd-2 --port=18885 --timeout=0
 
 exec /usr/local/bin/elementsd $@
--- elementsd-2/docker-entrypoint.sh
+++ elementsd-2/docker-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
-
 wait-for-it.sh --host=bitcoind --port=18888 --timeout=0
+wait-for-it.sh --host=elementsd-1 --port=18884 --timeout=0
 
 exec /usr/local/bin/elementsd $@
